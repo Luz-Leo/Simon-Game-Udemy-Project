cf17da0dad9db2b04dabe57f363a92e82e5c29de
Luz-Leo <leeonardoluz@gmail.com>
2023-06-01 12:41:46 +1000

    Dificulty, changed button.

--- game.js
+++ game.js
@@ -4,16 +4,10 @@ var gamePattern = [];
 var userClickedPattern = [];
 var started = false;
 var level = 0;
+var dificulty = 500;
 
-$(document).keypress(function () {
-    if (!started) {
-        $("#level-title").text("Level " + level);
-        nextSequence();
-        started = true;
-    }
-})
 
-$("body").click(function () {
+$("#level-title").click(function () {
     if (!started) {
         $("#level-title").text("Level " + level);
         nextSequence();
@@ -22,40 +16,43 @@ $("body").click(function () {
 })
 
 $(".btn").click(function () {
-    if (started == true){
+    if (started == true) {
         var userChosenColour = $(this).attr("id");
         userClickedPattern.push(userChosenColour);
-
-        playSound(userChosenColour);
-        animatePress(userChosenColour);
-        checkAnswer(userClickedPattern.length - 1);
-    }
+            if (userClickedPattern.length <= gamePattern.length) {
+                playSound(userChosenColour);
+                animatePress(userChosenColour);
+                checkAnswer(userClickedPattern.length - 1);
+            } else {
+                return;
+            }
+        }
 });
 
-function flashBox(num){
-    if (num < gamePattern.length){
-      
+function flashBox(num) {
+    if (num < gamePattern.length) {
+
         $('#' + gamePattern[num]).fadeOut(100).fadeIn(100);
         playSound(gamePattern[num]);
 
-        setTimeout(function() {
-            console.log('delay');
-            setTimeout(function(){
+        setTimeout(function () {
+            setTimeout(function () {
                 flashBox(++num);
                 console.log(num);
             }, 0);
-        }, 500);
-        
-    }else if(0 < gamePattern.length){
-       return;
-    }else{
+        }, dificulty);
+
+    } else if (0 < gamePattern.length) {
+        return;
+    } else {
         $('#' + gamePattern[num]).fadeOut(100).fadeIn(100);
         return;
     }
 }
 
 function nextSequence() {
-
+    dificulty = dificulty * 0.90;
+    console.log(dificulty);
     userClickedPattern = [];
     level++;
 
@@ -72,7 +69,7 @@ function nextSequence() {
 function playSound(name) {
 
     var audio = new Audio("sounds/" + name + ".mp3");
-    audio.volume = 0.1; 
+    audio.volume = 0.1;
     audio.play();
 }
 
@@ -89,12 +86,15 @@ function checkAnswer(currentLevel) {
     if (gamePattern[currentLevel] == userClickedPattern[currentLevel]) {
         console.log('Success');
         if (userClickedPattern.length == gamePattern.length) {
-
             setTimeout(function () {
+                console.log('initialize 3');
                 nextSequence();
             }, 1000);
         }
     } else {
+
+        console.log("Game Length: " + gamePattern.length);
+        console.log('Click pattern: ' + userClickedPattern.length);
         console.log("wrong");
         playSound("wrong");
 
@@ -103,7 +103,7 @@ function checkAnswer(currentLevel) {
             $('body').removeClass('game-over');
         }, 200);
 
-        $("#level-title").text("Game Over, Press Any Key to Restart");
+        $("#level-title").text("Press to RESTART");
         startOver();
     }
 
@@ -111,9 +111,11 @@ function checkAnswer(currentLevel) {
 
 
 function startOver() {
+
     started = false;
     gamePattern = [];
     level = 0;
+    dificulty = 500
 }
 
 
